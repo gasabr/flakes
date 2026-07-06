80f5f04dfada223a5e482ab36bf8e855d90aff5a
Gleb Abroskin <gleb@podscribe.com>
2026-07-06 09:02:36 -0700

    cli-tools: added abbreviations

--- cli-tools/flake.nix
+++ cli-tools/flake.nix
@@ -10,7 +10,17 @@
         in {
           default = pkgs.mkShell {
             buildInputs = [ pkgs.git pkgs.jq pkgs.lazygit pkgs.ripgrep pkgs.k9s pkgs.kubectl pkgs.doctl pkgs.cursor-cli pkgs.github-cli pkgs.clickhouse pkgs.pgcli pkgs.mdformat ];
-            shellHook = ''if [[ $- == *i* ]]; then exec fish; fi'';
+            shellHook = ''
+              if [[ $- == *i* ]]; then
+                exec fish --init-command '
+                  abbr -a gp "git pull"
+                  abbr -a gst "git status"
+                  abbr -a gf "git fetch"
+                  abbr -a lg "lazygit"
+                  abbr -a dc "docker-compose"
+                '
+              fi
+            '';
           };
         });
     };
